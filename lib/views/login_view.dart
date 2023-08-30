529cc44a08d8a47281dab88940386ee09669dc03
ARI <nikitos52@gmail.com>
2023-08-31 00:49:23 +0300

    step-16

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notsify/constants/routes.dart';
 import 'package:notsify/services/auth/auth_exceptions.dart';
 import 'package:notsify/services/auth/auth_service.dart';
-import 'package:notsify/utilities/show_error_dialog.dart';
+import 'package:notsify/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
